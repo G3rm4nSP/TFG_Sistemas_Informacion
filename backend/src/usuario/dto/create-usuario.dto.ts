47814995644bb2cc52ba31f27c9ddfdbc8a908a0
G3rm4nSP <g.sportillo@alumnos.upm.es>
2026-02-18 17:25:31 +0100

    Añadimos un seed y arreglamos dt de usuario

--- backend/src/usuario/dto/create-usuario.dto.ts
+++ backend/src/usuario/dto/create-usuario.dto.ts
@@ -1,10 +1,12 @@
+import { Rol } from '@prisma/client';
 export class CreateUsuarioDto {
     empleadoId! : string;
     mail! : string;
     passwordHash! : string;
-    rol! : string;
+    rol! : Rol;
     ultimoAcceso! : Date;
     activo? : boolean;
     intentosFallidos! : number;
     bloqueadoHasta! : Date;
 }
+
